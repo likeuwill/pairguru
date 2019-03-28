efd0211aa1e09f08a58ba89ee7ab9d93fe81f266
Sergii Chub <likeuwill@yahoo.com>
2019-03-28 21:23:49 +0100

    add movies controllers for v1 and v2 modules

---
+++ app/controllers/api/v1/movies_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    class MoviesController < ApplicationController
+      before_action :set_movie, only: [:show]
+
+      def index
+        @movies = Movie.all
+      end
+
+      def show; end
+
+      private
+
+      def set_movie
+        @movie = Movie.find(params[:id])
+      end
+    end
+  end
+end
